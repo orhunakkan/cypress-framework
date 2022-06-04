feaf7f82afd851b963d6411139cde992ec86659c
Orhun Akkan <orhunakkan@outlook.com>
2022-06-04 02:44:36 -0500

    trying to retain html test results in circleCI

--- cypress.config.js
+++ cypress.config.js
@@ -24,7 +24,7 @@ module.exports = defineConfig({
       reportDir: "cypress/reports/mocha",
       quite: true,
       overwrite: false,
-      html: false,
+      html: true,
       json: true,
     },
   },
